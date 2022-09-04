7ed17b906c3073b2969637c48003f0ebe6d75dd0
Danil <danila_monster@mail,ru>
2022-09-04 21:53:30 +0300

    Task68

---
+++ DZ003/Program.cs
@@ -0,0 +1,25 @@
+﻿//  Задача 68: Напишите программу вычисления функции
+//  Аккермана с помощью рекурсии. Даны два 
+//  неотрицательных числа m и n. 
+//  m = 2, n = 3 -> A(m,n) = 29
+
+
+int RecFunAkker(int m, int n)
+{
+    // Базовый случай
+    if (m == 0)
+    {
+        return n + 1;
+    } // Шаг рекурсии / рекурсивное условие
+    else if (n == 0 && m > 0)
+    {
+        return RecFunAkker(m - 1, 1);
+    } // Шаг рекурсии / рекурсивное условие
+    else
+    {
+        return RecFunAkker(m - 1, RecFunAkker(m, n - 1));
+    }
+}
+
+Console.WriteLine(RecFunAkker(3, 2)); 
+
